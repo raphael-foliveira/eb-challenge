1ab13339eb68f979b938e62c7f37c03af48df0f7
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-06-09 14:54:31 -0300

    add api status check on swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   @Get()
+  @ApiResponse({ status: 200, description: 'API Status Check', type: String })
   statusCheck(): string {
     return 'Ok';
   }
